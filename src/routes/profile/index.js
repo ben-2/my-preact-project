e16df2652a0115e30ab854f9c891c1f93205bb48
Ben D <bdek79@gmail.com>
2020-03-17 17:58:22 +0100

    Transform preact class component to function component

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,47 +1,38 @@
 import { h, Component } from 'preact';
+import { useState, useEffect } from 'preact/hooks'
 import style from './style';
 
-export default class Profile extends Component {
-	state = {
-		time: Date.now(),
-		count: 10
-	};
+const Profile = (props) => {
+	const [time, setTime] = useState(Date.now());
+	const [count, setCount] = useState(10);
 
+	const { user } = props;
 	// update the current time
-	updateTime = () => {
-		this.setState({ time: Date.now() });
+	const increment = () => {
+		setCount(count+1);
 	};
-
-	increment = () => {
-		this.setState({ count: this.state.count+1 });
-	};
-
-	// gets called when this route is navigated to
-	componentDidMount() {
+	useEffect(() => {
 		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
-	}
-
-	// gets called just before navigating away from the route
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
+		const timer = setInterval(setTime(Date.now()), 1000);
+		return () => {
+			clearInterval(timer);
+		}
+	}, [])
 
 	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
-		return (
-			<div class={style.profile}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
+	return (
+		<div class={style.profile}>
+			<h1>Profile: </h1>
+			<p>This is the user profile for a user named {user}.</p>
 
-				<div>Current time: {new Date(time).toLocaleString()}</div>
+			<div>Current time: {new Date(time).toLocaleString()}</div>
 
-				<p>
-					<button onClick={this.increment}>Click Me</button>
-					{' '}
-					Clicked {count} times.
-				</p>
-			</div>
-		);
-	}
+			<p>
+				<button onClick={() => increment()}>Click Me</button>
+				{' '}
+				Clicked {count} times.
+			</p>
+		</div>
+	);
 }
+export default Profile;
